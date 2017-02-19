e96b45c65822afd16ef564e074b133198ae756c6
beosdev <blhl1210@gmail.com>
2017-02-19 14:41:43 +0700

    Fix Code
    Add comment

--- ProjectApp/CfManage/Program.cs
+++ ProjectApp/CfManage/Program.cs
@@ -14,6 +14,7 @@ namespace CfManage
         [STAThread]
         static void Main()
         {
+            //Hello
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Login());
